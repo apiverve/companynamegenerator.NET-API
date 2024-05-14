ab41bf29122c794ff3793efb834380f6512a0ba5
APIVerve Team <hello@apiverve.com>
2024-05-14 15:22:55 -0500

    Bug fixes and Improvements

---
+++ companynamegeneratorQueryOptions.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class companynamegeneratorQueryOptions {
+public string keyword { get; set; }
+public int count { get; set; }
+}
+
+}
